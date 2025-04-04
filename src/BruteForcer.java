db5f31cb83832344bb87337e383ba6a7801fbc8f
”Artem <26and06@gmail.com>
2025-04-04 14:32:48 +0300

    without request about encrypted text

--- src/BruteForcer.java
+++ src/BruteForcer.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class BruteForcer {
+class BruteForcer {
+    private final CaesarCipher cipher;
+
+    public BruteForcer() {
+        this.cipher = new CaesarCipher();
+    }
+
+    public void attemptDecryption(String encryptedText, String outputPath) {
+        try {
+            Writer writer = new Writer();
+            for (int shift = 0; shift < 26; shift++) {
+                String decrypted = cipher.decrypt(encryptedText, shift);
+                String filePath = outputPath + "_shift" + shift + ".txt";
+                writer.write(filePath, decrypted);
+                System.out.println("Сохранен вариант с ключом " + shift + ": " + filePath);
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при брутфорсе: " + e.getMessage());
+        }
+    }
 }
