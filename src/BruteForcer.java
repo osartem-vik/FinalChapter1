e6427c9395d83129363e38f606d84f81a1d0a089
”Artem <26and06@gmail.com>
2025-04-11 09:47:15 +0300

    all comments were deleted

--- src/BruteForcer.java
+++ src/BruteForcer.java
@@ -5,21 +5,19 @@ import java.util.Scanner;
 
 public class BruteForcer {
     private final CaesarCipher cipher;
-    private final int MOST_FREQ_LETTER_LAT = 101;  //latin 'e' in UNICODE
-    private final int MOST_FREQ_LETTER_CYR = 1086; //cyrillic 'o' in UNICODE
-    private final int QUANTITY_LETTER_CYR = 32; //quantity cyrillic letters in UNICODE
-    private final int QUANTITY_LETTER_LAT = 26; //quantity latin letters in UNICODE
-    private final char FIRST_LETTER_LAT = 'A'; //first letter of latin
-    private final char FIRST_SMALL_LETTER_LAT = 'a'; //first small letter in latin
-    private final char FIRST_LETTER_CYR = 'А'; //first letter in cyrillic
-    private final char FIRST_SMALL_LETTER_CYR = 'а'; //first small letter in cyrillic
-//    private final String ukrainianAlphabet = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя";
+    private final int MOST_FREQ_LETTER_LAT = 101;
+    private final int MOST_FREQ_LETTER_CYR = 1086;
+    private final int QUANTITY_LETTER_CYR = 32;
+    private final int QUANTITY_LETTER_LAT = 26;
+    private final char FIRST_LETTER_LAT = 'A';
+    private final char FIRST_SMALL_LETTER_LAT = 'a';
+    private final char FIRST_LETTER_CYR = 'А';
+    private final char FIRST_SMALL_LETTER_CYR = 'а';
     public BruteForcer() {
         this.cipher = new CaesarCipher();
     }
 
-    /*The method iteratively selects a key with the ability to save the final file
-in case of a positive result*/
+
     public void attemptDecryption(String encryptedText, String outputPath) {
         IOFile IOFile = new IOFile();
         int quantAttempt = isLatinText(mostFrequencyChar(encryptedText)) ? QUANTITY_LETTER_LAT : QUANTITY_LETTER_CYR;
@@ -47,17 +45,14 @@ in case of a positive result*/
         System.out.println("Brute-force completed");
     }
 
-    // The method is designed to calculate the most frequently occurring symbol in the text
     public char mostFrequencyChar(String text) {
         Map<Character, Integer> frequency = new HashMap<>();
         char mostFrequent = 0;
         int maxCount = 0;
-        //Determining the frequency of each character in the text
         for (char ch : text.toLowerCase().toCharArray()) {
             if (Character.isLetter(ch))
                 frequency.put(ch, frequency.getOrDefault(ch, 0) + 1);
         }
-        //Output of the most frequently occurring symbol
         for (Map.Entry<Character, Integer> entry : frequency.entrySet()) {
             if (entry.getValue() > maxCount) {
                 mostFrequent = entry.getKey();
@@ -66,7 +61,6 @@ in case of a positive result*/
         }
         return mostFrequent;
     }
-    //The method for determining a key based on frequency analysis
     private String stringProcessor(String encryptedText) {
         StringBuilder result = new StringBuilder();
         int shift = -1;
@@ -97,7 +91,6 @@ in case of a positive result*/
         return result.toString();
     }
 
-    // Definition of text Latin or Cyrillic
     private boolean isLatinText(char mostFreqChar) {
         return mostFreqChar >= 65 && mostFreqChar <= 122;
     }
@@ -106,7 +99,6 @@ in case of a positive result*/
         return mostFreqChar >= 1024 && mostFreqChar <= 1279;
     }
 
-    //The method automaticaly decrypts text and saves it
     public void automaticalyBrut(String encryptedText, String outputPath) {
         if (encryptedText == null || encryptedText.isEmpty()) {
             System.out.println("The file is empty");
